183bf56c277a6e7126ab04cfcf5b7056e0950ef5
Afrie Irham <afrieirham.work@gmail.com>
2025-06-23 00:34:57 +0800

    add search input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,23 @@
+import Form from "next/form";
 import Link from "next/link";
 
 import { formatDistanceToNow } from "date-fns";
 import parse from "html-react-parser";
 
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { api, HydrateClient } from "@/trpc/server";
 
-export default async function Home() {
-  const jobs = await api.job.getAll();
+type Props = { searchParams: { query?: string } };
+
+export default async function Home({ searchParams }: Props) {
+  const query = searchParams.query ?? "";
+
+  const jobs = await api.job.getAll({ searchTerm: query });
 
   return (
     <HydrateClient>
-      <main className="mx-auto flex max-w-6xl flex-col p-4">
+      <main className="mx-auto max-w-6xl p-4">
         <h1 className="text-xl">Kerja-IT.com</h1>
         <p className="text-sm">
           IT jobs in Malaysia sourced from various job boards.{" "}
@@ -18,24 +25,45 @@ export default async function Home() {
             (yes new simplified design)
           </span>
         </p>
-        <div>
-          {jobs.map((job) => (
-            <div key={job.id} className="mt-8">
-              <Link
-                href={job.url}
-                target="_blank"
-                className="text-blue-500 visited:text-purple-900 hover:underline"
-              >
-                {job.title}
-              </Link>
-              <p className="text-xs text-gray-500">
-                {formatDistanceToNow(job.createdAt)} ago • {job.source}
-              </p>
-              <p className="mt-2 text-sm text-gray-800">
-                {parse(job.description)}
-              </p>
-            </div>
-          ))}
+
+        <Form action="/" className="mt-4 flex items-center gap-2">
+          <Input
+            name="query"
+            placeholder="Search: Software Engineer"
+            className="max-w-md"
+            type="search"
+            defaultValue={query}
+          />
+          <Button type="submit">Search</Button>
+          {Boolean(query) && (
+            <Button variant="ghost" asChild>
+              {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+              <a href="/">Reset</a>
+            </Button>
+          )}
+        </Form>
+        <div className="mt-4">
+          {jobs.length > 0 ? (
+            jobs.map((job) => (
+              <div key={job.id} className="mb-8">
+                <Link
+                  href={job.url}
+                  target="_blank"
+                  className="text-blue-500 visited:text-purple-900 hover:underline"
+                >
+                  {job.title}
+                </Link>
+                <p className="text-xs text-gray-500">
+                  {formatDistanceToNow(job.createdAt)} ago • {job.source}
+                </p>
+                <p className="mt-2 text-sm text-gray-800">
+                  {parse(job.description)}
+                </p>
+              </div>
+            ))
+          ) : (
+            <p className="text-sm text-gray-800">No jobs found.</p>
+          )}
         </div>
       </main>
     </HydrateClient>
--- src/server/api/routers/job.ts
+++ src/server/api/routers/job.ts
@@ -3,7 +3,28 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
 export const jobRouter = createTRPCRouter({
-  getAll: publicProcedure.query(async ({ ctx }) => {
-    return ctx.db.job.findMany({ orderBy: { createdAt: "desc" } });
-  }),
+  getAll: publicProcedure
+    .input(z.object({ searchTerm: z.string().optional() }))
+    .query(async ({ ctx, input }) => {
+      const { searchTerm } = input;
+      return ctx.db.job.findMany({
+        orderBy: { createdAt: "desc" },
+        where: {
+          OR: [
+            {
+              title: {
+                contains: searchTerm,
+                mode: "insensitive",
+              },
+            },
+            {
+              description: {
+                contains: searchTerm,
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+      });
+    }),
 });
